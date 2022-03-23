7a2ebab27c4e5e39edfdfa63f7ab46f6d56d10fe
robin ucar <mehmet.ucar@imediapro.co.uk>
2022-03-23 16:51:56 +0000

    seeing bookmarks

--- spec/features/viewing_bookmarks_spec.rb
+++ spec/features/viewing_bookmarks_spec.rb
@@ -3,4 +3,11 @@ feature 'Viewing bookmarks' do
     visit('/')
     expect(page).to have_content "Bookmark Manager"
   end
-end
+
+  scenario 'A user can see bookmarks' do
+    visit('/bookmarks')
+    expect(page).to have_content "http://www.makersacademy.com"
+    expect(page).to have_content "http://www.destroyallsoftware.com"
+    expect(page).to have_content "http://google.com"
+  end
+end 
